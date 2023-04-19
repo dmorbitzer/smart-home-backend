02264ee0faeb7c79a025c28c91b661f5983b9f8b
morbitzerd <d.morbitzer@techdivision.com>
2023-04-19 19:13:16 +0200

    SH-22: Implement CatFeedingServiceHandler

--- src/Handler/CatFeedingServiceHandler.php
+++ src/Handler/CatFeedingServiceHandler.php
@@ -3,20 +3,62 @@
 namespace App\Handler;
 
 use App\Message\CatFeedingServiceMessage;
+use App\Repository\CatRepository;
+use App\Repository\FoodRepository;
+use App\Repository\ServiceRepository;
+use App\Service\LogServiceInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+use function Symfony\Component\Clock\now;
 
 #[AsMessageHandler]
 class CatFeedingServiceHandler
 {
+    /** @var LogServiceInterface  */
+    private LogServiceInterface $logService;
+
+    /** @var ServiceRepository  */
+    private ServiceRepository $serviceRepository;
+
+    /** @var CatRepository  */
+    private CatRepository $catRepository;
+
+    /** @var FoodRepository  */
+    private FoodRepository $foodRepository;
+
+    /**
+     * @param LogServiceInterface $logService
+     * @param ServiceRepository $serviceRepository
+     * @param CatRepository $catRepository
+     * @param FoodRepository $foodRepository
+     */
+    public function __construct(
+        LogServiceInterface $logService,
+        ServiceRepository $serviceRepository,
+        CatRepository $catRepository,
+        FoodRepository $foodRepository
+    ) {
+        $this->logService = $logService;
+        $this->serviceRepository = $serviceRepository;
+        $this->catRepository = $catRepository;
+        $this->foodRepository = $foodRepository;
+    }
+
+    /**
+     * @param CatFeedingServiceMessage $message
+     */
     public function __invoke(CatFeedingServiceMessage $message)
     {
-        print_r(
-            'Handler handled the feeding message with catId ' .
-            $message->getCatId() .
-            ' and foodId ' .
-            $message->getFoodId() .
-            ' and type: ' .
-            $message->getType()
-        );
+        $cat = $this->catRepository->find($message->getCatId());
+        $food = $this->foodRepository->find($message->getFoodId());
+        $service = $this->serviceRepository->findOneBy(['identifier' => 'cat_feeding']);
+
+        $data = [
+            'cat' => $cat->getName(),
+            'foodType' => $food->getName(),
+            'type' => $message->getType()
+        ];
+
+        $this->logService->log($service, now(), $data);
     }
 }
